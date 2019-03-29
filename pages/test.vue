6e8593e86a4ff38fa75131c1018559d7a2efa1a6
DEVELOPER1-DESC\aismagilov <aismagilov@c-i-p.ru>
2019-03-29 18:40:51 +0300

    add test component

---
+++ pages/test.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>{{ msg }}</div>
+</template>
+
+<script>
+  export default {
+    name: 'test component',
+    data() {
+      return {
+        msg: 'Hello, i am test message'
+      }
+    }
+  }
+</script>
