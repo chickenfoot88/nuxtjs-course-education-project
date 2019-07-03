c9273b781d98aa24730272f92aa3df6e9eb14cec
chickenfoot88 <chikenfoot88@gmail.com>
2019-07-03 07:24:29 +0300

    add about page

---
+++ pages/about/index.vue
@@ -0,0 +1,29 @@
+<template lang='pug'>
+  div.about-page
+    h1 This is my absolutely fantastic "About Me" Page!
+    p Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam officiis ab voluptatibus assumenda error quas deleniti eius? Suscipit, numquam? Quis aliquid non animi numquam amet voluptas dolorum sunt aspernatur commodi?
+    p Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam officiis ab voluptatibus assumenda error quas deleniti eius? Suscipit, numquam? Quis aliquid non animi numquam amet voluptas dolorum sunt aspernatur commodi?
+    p Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam officiis ab voluptatibus assumenda error quas deleniti eius? Suscipit, numquam? Quis aliquid non animi numquam amet voluptas dolorum sunt aspernatur commodi?
+    p Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam officiis ab voluptatibus assumenda error quas deleniti eius? Suscipit, numquam? Quis aliquid non animi numquam amet voluptas dolorum sunt aspernatur commodi?
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      exampleText: [
+        'lorem 1',
+        'lorem 2',
+        'lorem 2',
+        'lorem 3',
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped lang='sass'>
+  .about-page
+    text-align: center
+    padding: 30px
+</style>
