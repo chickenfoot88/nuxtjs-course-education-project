1c47d7f9385f5450d0904a2bb052728c952b9b45
chickenfoot88 <chikenfoot88@gmail.com>
2019-07-03 07:22:33 +0300

    add ui components

---
+++ components/UI/AppButton.vue
@@ -0,0 +1,48 @@
+<template lang='pug'>
+  button.button(:class="btnStyle" v-bind="$attrs" @="$listeners")
+    slot
+</template>
+
+<script>
+export default {
+  name: 'AppButton',
+  props: {
+    btnStyle: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+
+<style scoped lang='sass'>
+  .button
+    font: inherit
+    background-color: black
+    color: white
+    padding: 5px
+    cursor: pointer
+    border: 1px solid black
+    &:hover, &:active
+      background-color: rgb(51, 51, 51)
+      border: 1px solid black
+
+  .inverted
+    background-color: white
+    color: black
+    border: 1px solid transparent
+    &:hover, &:active
+      color: #ccc
+      background-color: white
+      border: 1px solid transparent
+
+  .cancel
+    background-color: red
+    color: white
+    border: 1px solid transparent
+    &:hover, &:active
+      background-color: salmon
+      border: 1px solid transparent
+      color: white
+</style>
---
+++ components/UI/AppControlInput.vue
@@ -0,0 +1,52 @@
+<template lang='pug'>
+  .input-control
+    label
+      slot
+    input(
+      v-if='controType === "input"'
+      v-bind="$attrs"
+      :value='value'
+      @input='$emit("input", $event.target.value)')
+    textarea(
+      v-if='controType === "textarea"'
+      rows='10'
+      :value='value'
+      @input='$emit("input", $event.target.value)')
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppInputControl',
+  props: {
+    controlType: {
+      type: String,
+      default: 'input'
+    },
+    value: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped lang='sass'>
+  .input-control
+    margin: 10px 0
+    label
+      display: block
+      font-weight: bold
+    input, textarea
+      display: block
+      width: 100%
+      box-sizing: border-box
+      font: inherit
+      border: 1px solid #ccc
+      padding: 5px
+    input:focus, textarea:focus
+      background-color: #eee
+      outline: none
+</style>
+
+
