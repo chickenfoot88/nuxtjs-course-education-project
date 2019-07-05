8679323eebd343961a931615f7f1036a62694a00
aismagilov <aismagilov@c-i-p.ru>
2019-07-05 07:16:22 +0300

    add error layout

---
+++ layouts/error.vue
@@ -0,0 +1,18 @@
+<template lang='pug'>
+  .error-page
+    h1 Oops, something went wrong!
+    p
+      | Back to 
+      a(href='/') safety
+      | !
+</template>
+
+<style scoped lang='sass'>
+.error-page
+  text-align: center
+  a
+    text-decoration: none
+    color: red
+    &:hover, &:active
+      color: salmon
+</style>
