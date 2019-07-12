48c831bc9626cc02ffe26d13d29f4fec7aa68f92
chickenfoot88 <chikenfoot88@gmail.com>
2019-07-12 08:02:27 +0300

    add preview field for post edit

--- components/admin/AdminPostForm.vue
+++ components/admin/AdminPostForm.vue
@@ -2,8 +2,9 @@
   form(@submit.prevent='onSave')
     AppControlInput(v-model='editedPost.author') Author Name
     AppControlInput(v-model='editedPost.title') Title
-    AppControlInput(v-model='editedPost.thumbnailLink') Thumbnail Link
+    AppControlInput(v-model='editedPost.thumbnail') Thumbnail Link
     AppControlInput(control-type='textarea' v-model='editedPost.content') Content
+    AppControlInput(control-type='textarea' v-model='editedPost.previewText') Preview Text
     AppButton(type='submit') Save
     AppButton(type='button' style='margin-left: 10px' btn-style='cancel' @click='onCancel') Cancel
 </template>
@@ -30,8 +31,9 @@ export default {
         : {
           author: '',
           title: '',
-          thumbnailLink: '',
-          content: ''
+          thumbnail: '',
+          content: '',
+          previewText: ''
         }
     }
   },
